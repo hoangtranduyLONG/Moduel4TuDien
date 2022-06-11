8bcc0e0fa8b002738127053bc8b80d502f506f42
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-06-11 17:06:03 +0700

    done

--- src/main/java/com/codegym/controller/Translate.java
+++ src/main/java/com/codegym/controller/Translate.java
@@ -1,37 +1,43 @@
 package com.codegym.controller;
-
-
 import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
-        import java.util.HashMap;
-        import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class Translate {
-    @GetMapping()
-    public String index() {
-        return "index";
+    Map<String, String> dic = new HashMap();
+
+    @GetMapping("")
+    public ModelAndView showList() {
+        return new ModelAndView("");
     }
 
-    @GetMapping("/translate")
-    public String trans(@RequestParam String key, Model model) {
-        Map<String, String> list = new HashMap<>();
-        list.put("generation", "thế hệ");
-        list.put("road", "đường");
-        list.put("mouse", "chuột");
-        list.put("engineer", "kỹ sư");
-        list.put("president", "tổng thống");
-        list.put("turkey", "thổ nhĩ kỳ");
-        list.put("korea", "hàn quốc");
-        list.put("bridge", "cầu");
-        list.put("moon", "mặt trăng");
-        if (list.containsKey(key)) {
-            model.addAttribute("rs", list.get(key));
-        } else
-            model.addAttribute("rs", "Not found");
-        return "result";
+    @PostMapping("/translate")
+    public ModelAndView translate(@RequestParam("english") String english) {
+        dic.put("House", "Ngôi nhà");
+        dic.put("Lion", "Sư tử");
+        dic.put("Engineer", "Kỹ Sư");
+        dic.put("Tea", "Trà");
+        dic.put("Coffee", "Cà Phê");
+        Set<String> key = dic.keySet();
+        ModelAndView modelAndView = new ModelAndView("index");
+        for (String k : key) {
+            if (k.equals(english)) {
+                modelAndView.addObject("TV", dic.get(k));
+                break;
+            }
+            else {
+                modelAndView.addObject("TV", "Not Found");
+            }
+        }
+        return modelAndView;
     }
+
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 11/06/2022
+  Time: 4:57 CH
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+<form action="/translate" method="post">
+    <input type="text" name="english">
+    <button type="submit">Translate</button>
+</form>
+Tiếng Việt: ${TV}
+</body>
+</html>
